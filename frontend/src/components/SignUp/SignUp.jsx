51f40371827746d304210d7cd8a04cb2d4429550
Daniel <zankhansql@gmail.com>
2025-07-08 17:17:15 +0530

    popup-changes

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -69,7 +69,7 @@ const SignUp = ({showLoginPopup, setShowLoginPopup}) => {
   }
 
   useEffect(() => {
-    if(!showLoginPopup){
+    if(!showLoginPopup || loginState){
       setDetails({
         username: "",
         email: "",
