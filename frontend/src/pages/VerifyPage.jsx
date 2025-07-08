cf4e0fe859fa06421879eda732fa6ddecd488838
Daniel <zankhansql@gmail.com>
2025-07-08 16:21:21 +0530

    border-color-change

--- frontend/src/pages/VerifyPage.jsx
+++ frontend/src/pages/VerifyPage.jsx
@@ -35,7 +35,7 @@ const VerifyPage = () => {
   }
   return token && transactionGoing ? (
     <div className='min-h-screen min-w-[100%] flex justify-center items-center'>
-      <div className='h-[100px] w-[100px] border-2 border-y-red-500 rounded-full animate-[loading_1s_linear_infinite]'>
+      <div className='h-[100px] w-[100px] border-2 border-y-red-500 border-x-transparent rounded-full animate-[loading_1s_linear_infinite]'>
 
       </div>
     </div>
